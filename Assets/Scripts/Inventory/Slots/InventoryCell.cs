b5b94b917efbfb00070c18fdc720fbc6687224a4
MrAngle <mr.angle69@gmail.com>
2025-10-19 23:09:39 +0200

    Init eq grid

---
+++ Assets/Scripts/Inventory/Slots/InventoryCell.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Inventory.Slots {
+    public enum CellState
+    {
+        Unreachable,  // nieosiągalne
+        Empty,        // dozwolone i puste
+        Occupied      // dozwolone ale zajęte
+        // (możesz dodać: Locked / Reserved)
+    }
+    public class InventoryCell {
+        public CellState State { get; set; }
+
+        public InventoryCell(CellState state = CellState.Unreachable)
+        {
+            State = state;
+        }
+
+        public bool IsAvailableForPlacement => State == CellState.Empty;
+    }
+}
---
+++ Assets/Scripts/Inventory/Slots/InventoryGrid.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Inventory.Slots {
+    public class InventoryGrid {
+        private readonly Dictionary<Vector2Int, InventoryCell> _cells;
+
+        public InventoryGrid(int width, int height)
+        {
+            _cells = new Dictionary<Vector2Int, InventoryCell>();
+            for (int xIndex = 0; xIndex < width; xIndex++)
+            {
+                for (int yIndex = 0; yIndex < height; yIndex++)
+                {
+                    _cells[new Vector2Int(xIndex, yIndex)] = new InventoryCell();
+                }
+            }
+        }
+
+        public InventoryCell GetCell(Vector2Int pos)
+        {
+            _cells.TryGetValue(pos, out var cell);
+            return cell;
+        }
+
+        // public bool CanPlace(ItemShape shape, Vector2Int origin)
+        // {
+        //     foreach (var offset in shape.Cells)
+        //     {
+        //         var pos = origin + offset;
+        //         if (!_cells.ContainsKey(pos)) return false;
+        //         if (_cells[pos].State != CellState.Empty) return false;
+        //     }
+        //     return true;
+        // }
+    }
+}
